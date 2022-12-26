8d41ce1f832a08bed388458a55121809746cef87
yihsin9111 <b10901121@ntu.edu.tw>
2022-12-26 15:18:08 +0800

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,9 +8,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
-// if (process.env.NODE_ENV === "development") {
-// 	app.use(cors());
-// }
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
 app.use(express.json());
 
 //database
